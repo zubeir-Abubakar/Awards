03d2bc7904f109c0c0b49c707cf1109ad9fe1295
zubeir-Abubakar <zubkayare@gmail.com>
2019-08-04 14:57:44 +0300

    creating class project from .models into models

--- pro_jects/models.py
+++ pro_jects/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
-class Project(models.Model):
+class Projects(models.Model):
   name=models.CharField(max_length=40)
   image=models.ImageField(upload_to='projects/',default='tech.jpeg')
   user=models.ForeignKey(User,on_delete=models.CASCADE)
